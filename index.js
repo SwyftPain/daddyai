f6a34c20d87b51cbdbde38cbf2420b7fdc3055d0
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2023-10-19 20:49:06 +0200

    .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/files', express.static(path.join(__dirname, 'files')));
 
 app.get('/data', (req, res) => {
   return res.json({
-    Version: 0,
+    Version: 1,
     DownloadUrl: `http://localhost:${port}/files/debug.zip`
   });
 });
