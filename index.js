4fd9a7c1447987b2c79e911472d94c0deb289c43
SwyftPain <100738736+SwyftPain@users.noreply.github.com>
2023-10-28 18:50:31 +0200

    .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/files', express.static(path.join(__dirname, 'files')));
 
 app.get('/data', (req, res) => {
   return res.json({
-    Version: 1,
+    Version: 2,
     DownloadUrl: `https://github.com/SwyftPain/daddyai/releases/download/V1/debug.zip`
   });
 });
